e92131c0ee40948135ed6784fbc2bce421e73e39
Matheus Gonçalves <matheusgoncalvesmf@gmail.com>
2025-11-03 13:56:48 -0300

    Minificação dos arquivos

--- assets/js/utils/template-engine.js
+++ assets/js/utils/template-engine.js
@@ -1,14 +1,14 @@
-export const projectCardTemplate = ({ title, subtitle, description, imageUrl, altText }) => `
+export const projectCardTemplate=({title:t,subtitle:a,description:e,imageUrl:c,altText:l})=>`
     <article class="projeto col-span-4">
         <img 
-            src="${imageUrl}" 
-            alt="${altText}" 
+            src="${c}" 
+            alt="${l}" 
             width="500" 
             style="max-width: 100%; height: auto;"
         />
-        <h2>${title}</h2>
-        <p class="card-subtitle">${subtitle}</p>
+        <h2>${t}</h2>
+        <p class="card-subtitle">${a}</p>
         
-        <p>${description}</p>
+        <p>${e}</p>
     </article>
 `;
